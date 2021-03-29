7fa6e1ca8e57b694df19465f5623a6801e90d3d8
Szymon Leszkiewicz <szymon.leszkiewicz10@gmail.com>
2021-03-29 20:45:39 +0200

    programming 2 - done

--- 2021 - próbna/programming - galerie_handlowe/main.py
+++ 2021 - próbna/programming - galerie_handlowe/main.py
@@ -10,3 +10,25 @@ def pod1():
 
         for i in kraje.items():
             print(i)
+
+
+def podb():
+    with open('galerie.txt', 'r') as plik:
+        miasta = {}
+        for i in range(50):
+            dane = plik.readline().split()
+            # print('test ',dane)
+            miasto = dane[1]
+            dane = list(map(int, dane[2::]))
+            suma = 0
+            galerii = 0
+            for x in range(0, len(dane), 2):
+                if dane[x] == 0:
+                    break
+                lokal = dane[x] * dane[x + 1]
+                suma += lokal
+                galerii += 1
+            miasta[miasto] = suma, galerii
+
+        for i in miasta.items():
+            print(i)
