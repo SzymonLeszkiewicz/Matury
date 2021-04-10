0b9d20977c53d4b1440d74fdf99eaf8da06fdd85
Szymon Leszkiewicz <szymon.leszkiewicz10@gmail.com>
2021-04-10 21:53:29 +0200

    b)1

--- Matury stara formuła/2015/programming - słowa binarne/main.py
+++ Matury stara formuła/2015/programming - słowa binarne/main.py
@@ -1,3 +1,12 @@
+def bintodec(x):
+    suma = 0
+    potega = 1
+    for i in x[::-1]:
+        suma += potega * (ord(i) - 48)
+        potega *= 2
+    return suma
+
+
 def poda():
     with open("slowa.txt") as plik:
         x = plik.read().split()
@@ -8,3 +17,17 @@ def poda():
         with open("odp.txt", 'a') as odp:
             odp.write(f'a):\n')
             odp.write(f'{licznik}\n')
+
+
+def blok(x):
+    czy_przejscie = False
+
+    if x[0] == '1':
+        return False
+    for i in range(len(x) - 1):
+
+        if x[i] != x[i + 1] and czy_przejscie == False:
+            czy_przejscie = True
+        elif x[i] != x[i + 1] and czy_przejscie == True:
+            return False
+    return czy_przejscie
